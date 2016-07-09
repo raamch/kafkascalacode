d4092b53072ec653c3ece48dc4fb04488e71270b
raamch <raam.ch@gmail.com>
2016-07-09 23:56:48 +0100

    code

--- src/main/scala/KafkaCons.scala
+++ src/main/scala/KafkaCons.scala
@@ -7,12 +7,12 @@ import org.apache.kafka.clients.consumer.{ConsumerRecords, KafkaConsumer}
 object KafkaCons extends App{
   val props = new Properties()
   props.put("bootstrap.servers", "localhost:9092")
-  props.put("group.id", "kafka_test");
+  props.put("group.id", "test")
   props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
   props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
   val consumer = new KafkaConsumer[String, String] (props)
-  consumer.subscribe(util.Arrays.asList("kafka_test"))
+  consumer.subscribe(util.Arrays.asList("test"))
   while(true) {
     val records: ConsumerRecords[String, String] = consumer.poll(100)
     records.asScala.foreach(record => {
--- src/main/scala/KafkaProd.scala
+++ src/main/scala/KafkaProd.scala
@@ -1,13 +1,17 @@
 import java.util.Properties
+import scala.io.Source
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
 object KafkaProd extends App{
   val props = new Properties()
   props.put("bootstrap.servers", "localhost:9092")
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   val producer = new KafkaProducer[Integer, String](props)
-  for(cnt <- Range(0, 1000)) {
-    producer.send(new ProducerRecord[Integer, String] ("kafka_test","Message: "+cnt))
+
+  for (line <- Source.fromFile("TheTimeMachine.txt").getLines()) {
+    producer.send(new ProducerRecord[Integer, String] ("test",line))
   }
+  producer.send(new ProducerRecord[Integer, String] ("test","****Completed!****"))
   producer.close()
 }
